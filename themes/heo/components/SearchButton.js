a07ba1d0b9f293b17b049a1f5633c5ba435ea23f
carwlyjoe <130526582+carwlyjoe@users.noreply.github.com>
2024-10-06 01:52:46 +0800

    Update SearchButton.js

--- themes/heo/components/SearchButton.js
+++ themes/heo/components/SearchButton.js
@@ -23,10 +23,12 @@ export default function SearchButton(props) {
     }
   }
 
-  return <>
-        <div onClick={handleSearch} title={locale.NAV.SEARCH} alt={locale.NAV.SEARCH} className='cursor-pointer hover:bg-black hover:bg-opacity-10 rounded-full w-10 h-10 flex justify-center items-center duration-200 transition-all'>
-            <i title={locale.NAV.SEARCH} className="fa-solid fa-magnifying-glass" />
-        </div>
-        <AlgoliaSearchModal cRef={searchModal} {...props}/>
-    </>
+  return 
+  // 我的修改：去除多余的搜索图标
+  // <>
+  //       <div onClick={handleSearch} title={locale.NAV.SEARCH} alt={locale.NAV.SEARCH} className='cursor-pointer hover:bg-black hover:bg-opacity-10 rounded-full w-10 h-10 flex justify-center items-center duration-200 transition-all'>
+  //           <i title={locale.NAV.SEARCH} className="fa-solid fa-magnifying-glass" />
+  //       </div>
+  //       <AlgoliaSearchModal cRef={searchModal} {...props}/>
+  //   </>
 }
